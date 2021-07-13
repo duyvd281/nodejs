514dc2a0b3d5f62e6b81d17a7ffe6624f2ce07b0
duyvd <duy.vd176936@gmail.com>
2021-07-13 12:53:26 +0700

    MQTT publish in TOPIC: test

---
+++ mqttpub.js
@@ -0,0 +1,7 @@
+var mqtt = require('mqtt')
+var client = mqtt.connect('mqtt://127.0.0.1');
+
+client.on('connect',function(){
+    console.log('Connected')
+    client.publish('test','Duyvu test')
+})
