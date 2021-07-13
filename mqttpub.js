5e0891c7e3106ea67794f119a6ec576f113f0dbf
duyvd <duy.vd176936@gmail.com>
2021-07-13 15:21:25 +0700

    MQTT sub with event

--- mqttpub.js
+++ mqttpub.js
@@ -3,6 +3,6 @@ var client = mqtt.connect('mqtt://127.0.0.1');
 
 client.on('connect',function(){
     console.log('Connected')
-    client.publish('test','Duyvu test')
+    client.publish('test','Duyvu tes')
     client.end()
 })
---
+++ mqttsub.js
@@ -0,0 +1,23 @@
+var mqtt = require('mqtt')
+var client = mqtt.connect('mqtt://127.0.0.1')
+
+var Event = require('events')
+var Emiter1 = new Event.EventEmitter()
+var Emiter2 = new Event.EventEmitter()
+client.on('connect',function(){
+    client.subscribe('test');
+})
+client.on('message',function(topic,payload){
+    console.log(payload.toString())
+    if(payload == 'Duyvu test') Emiter1.emit('1');
+    else Emiter2.emit('1');
+    if(payload == 'close') client.end();
+})
+
+Emiter1.addListener('1',function(){
+    console.log('emit1 is running')
+})
+
+Emiter2.addListener('1', function(){
+    console.log('emit2 is running')
+    })
