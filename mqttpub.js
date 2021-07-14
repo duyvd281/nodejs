4490da54b6b12ecd89038c592d221150952425e5
duyvd <duy.vd176936@gmail.com>
2021-07-14 14:13:05 +0700

    publish message with input from stdin

--- mqttpub.js
+++ mqttpub.js
@@ -1,8 +1,15 @@
-var mqtt = require('mqtt')
+var mqtt = require('mqtt');
 var client = mqtt.connect('mqtt://127.0.0.1');
-
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+   
 client.on('connect',function(){
     console.log('Connected')
-    client.publish('test','Duyvu tes')
-    client.end()
+    readline.question('Type your message \n', message => {
+        client.publish('test',message);
+        client.end();
+        readline.close();
+      });
 })
--- mqttsub.js
+++ mqttsub.js
@@ -2,6 +2,7 @@ var mqtt = require('mqtt')
 var client = mqtt.connect('mqtt://127.0.0.1')
 
 var Event = require('events')
+const { exec } = require('child_process')
 var Emiter1 = new Event.EventEmitter()
 var Emiter2 = new Event.EventEmitter()
 client.on('connect',function(){
@@ -11,6 +12,7 @@ client.on('message',function(topic,payload){
     console.log(payload.toString())
     if(payload == 'Duyvu test') Emiter1.emit('1');
     else Emiter2.emit('1');
+    if(payload == 'stop mosquitto') exec('service mosquitto stop');
     if(payload == 'close') client.end();
 })
 
@@ -21,3 +23,8 @@ Emiter1.addListener('1',function(){
 Emiter2.addListener('1', function(){
     console.log('emit2 is running')
     })
+
+client.on('disconnect',function(){
+    console('Disconnect...')
+    client.end();
+})
