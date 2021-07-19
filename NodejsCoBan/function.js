28a705f804192474a7e176f29db239ae22e29bda
duyvd <duy.vd176936@gmail.com>
2021-07-19 10:05:12 +0700

    add function check to function.js

--- NodejsCoBan/function.js
+++ NodejsCoBan/function.js
@@ -1,14 +1,25 @@
 //This function use to calculate Area and Peripheral of the Rectangle
 function area(x,y){
+    if(check(x,y))
     return(x*y);
+    else return 0;
 }
 
 function per(x,y){
+    if(check(x,y))
     return((x+y)*2);
+    else return 0;
 }
 
+function check(x,y){
+    if (x<0 || y<0) {
+        console.log("Invalid Variable");
+        return 0;
+    }
+    else return 1;
+}
 //input variable
-x = 10;
+x = -10;
 y = 20;
 
 //Show the result
