6d97d4718003b4e815122dd562cd605d69f85dea
duyvd <duy.vd176936@gmail.com>
2021-07-19 10:23:18 +0700

    add notes

--- MQTTpubsub/mqttsub.js
+++ MQTTpubsub/mqttsub.js
@@ -1,13 +1,20 @@
+//Add API
 var mqtt = require('mqtt')
 var client = mqtt.connect('mqtt://127.0.0.1')
 
 var Event = require('events')
 const { exec } = require('child_process')
+
+//Add 2 event to handle the message
 var Emiter1 = new Event.EventEmitter()
 var Emiter2 = new Event.EventEmitter()
+
+//Connect to broker and subscribe
 client.on('connect',function(){
     client.subscribe('test');
 })
+
+//Get message and handle
 client.on('message',function(topic,payload){
     console.log(payload.toString())
     if(payload == 'Duyvu test') Emiter1.emit('1');
@@ -16,6 +23,7 @@ client.on('message',function(topic,payload){
     if(payload == 'close') client.end();
 })
 
+//Set events handle function
 Emiter1.addListener('1',function(){
     console.log('emit1 is running')
 })
